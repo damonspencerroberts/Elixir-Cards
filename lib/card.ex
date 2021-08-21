90587f2a5cba9146fcb0c410e6d7dad23d5adf91
Damon Roberts <damon@weekendr.eu>
2021-08-21 13:47:32 +0200

    finish cards module

--- lib/card.ex
+++ lib/card.ex
@@ -1,6 +1,21 @@
 defmodule Card do
   def create_cards do
-    cards = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    cards = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
     suits = ["Hearts", "Clubs", "Spades", "Diamonds"]
 
     for suit <- suits, card <- cards do
@@ -14,5 +29,30 @@ defmodule Card do
 
   def deal(cards, amt) do
     Enum.split(cards, amt)
+  end
+
+  def contains?(cards, card) do
+    Enum.member?(cards, card)
+  end
+
+  def save(cards, filename) do
+    binary = :erlang.term_to_binary(cards)
+    File.write(filename, binary)
+  end
+
+  def open(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _report} -> "There is no file that exists with #{filename}"
+    end
+  end
+
+  def draw do
+    {cards, _rest} =
+      Card.create_cards()
+      |> Card.shuffle()
+      |> Card.deal(5)
+
+    cards
   end
 end
